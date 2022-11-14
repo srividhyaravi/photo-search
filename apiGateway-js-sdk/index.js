00b4bd0129d63374d7be3246f3616ac833ae3dbe
Srividhya Ravi <40314454+srividhyaravi@users.noreply.github.com>
2022-11-14 18:33:01 -0500

    Add files via upload

--- apiGateway-js-sdk/index.js
+++ apiGateway-js-sdk/index.js
@@ -1,87 +1,267 @@
-var apigClient = apigClientFactory.newClient({apiKey: "aABGiAiHfW5sYOpOejBFQCtOnhWJ1iw3oTQrgpi0"});
+var apigClient = apigClientFactory.newClient();
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
 
-function textSearch() {
-    var searchboxtext = document.getElementById('searchterm');
-    if (!searchboxtext.value) {
-        alert('Please enter some text or voice input');
-    } 
-    else {
-        searchboxtext = searchboxtext.value.trim().toLowerCase();
-        searchPhotos(searchboxtext);
+function voiceSearch(){
+    if ('SpeechRecognition' in window) {
+        console.log("SpeechRecognition is Working");
+    } else {
+        console.log("SpeechRecognition is Not Working");
     }
-}
+    
+    var inputSearchQuery = document.getElementById("search_query");
+    const recognition = new window.SpeechRecognition();
+    //recognition.continuous = true;
 
-function speechRecognize() {    
-    var searchboxtext = document.getElementById("searchterm");
-    var recognition = new window.SpeechRecognition();
-    recognition.onstart = function() {
-        searchboxtext.value = "Listening ...";
-    };
-    recognition.onspeechend = function() {
+    micButton = document.getElementById("mic_search");  
+    
+    if (micButton.innerHTML == "mic") {
+        recognition.start();
+    } else if (micButton.innerHTML == "mic_off"){
         recognition.stop();
     }
-    recognition.onresult = function(event) {
-        var transcript = event.results[0][0].transcript;
-        searchboxtext.value = transcript;
+
+    recognition.addEventListener("start", function() {
+        micButton.innerHTML = "mic_off";
+        console.log("Recording.....");
+    });
+
+    recognition.addEventListener("end", function() {
+        console.log("Stopping recording.");
+        micButton.innerHTML = "mic";
+    });
+
+    recognition.addEventListener("result", resultOfSpeechRecognition);
+    function resultOfSpeechRecognition(event) {
+        const current = event.resultIndex;
+        transcript = event.results[current][0].transcript;
+        inputSearchQuery.value = transcript;
         console.log("transcript : ", transcript)
-    };
-    recognition.start();
+    }
 }
 
-function searchPhotos(searchboxtext) {
-    document.getElementById('upload-message').innerHTML = "";
-    document.getElementById('searchterm').value = searchboxtext;
-    var params = {'q' : searchboxtext, "x-api-key": "aABGiAiHfW5sYOpOejBFQCtOnhWJ1iw3oTQrgpi0"};
-    console.log("Params: ", params);
-    apigClient.searchGet(params)
+
+
+
+function textSearch() {
+    var searchText = document.getElementById('search_query');
+    if (!searchText.value) {
+        alert('Please enter a valid text or voice input!');
+    } else {
+        searchText = searchText.value.trim().toLowerCase();
+        console.log('Searching Photos....');
+        searchPhotos(searchText);
+    }
+    
+}
+
+function searchPhotos(searchText) {
+
+    console.log(searchText);
+    document.getElementById('search_query').value = searchText;
+    document.getElementById('photos_search_results').innerHTML = "<h4 style=\"text-align:center\">";
+
+    var params = {
+        'q' : searchText
+    };
+    
+    apigClient.searchGet(params, {}, {})
         .then(function(result) {
-            var photosresults = document.getElementById("photos-results-list");
-            photosresults.innerHTML = "";
-            if (result["data"] == "No Results found") {
-                photosresults.innerHTML = "No Results Found!"
-            }
-            else {
-                imgpaths = result["data"]["imagePaths"];
-                for (let i = 0; i < imgpaths.length; i++) {
-                    imglist = imgpaths[i].split('/');
-                    imgname = imglist[imglist.length - 1];
-                    let result = $("<div></div>").addClass("col-md-3")
-                    result.append('<figure><img src="' + imgpaths[i] + '" style="height:200px; width:250px;"><figcaption style="text-align:center;">' + imgname + '</figcaption></figure>')
-                    photosresults.innerHTML += result.html()
-                }
+            console.log("Result : ", result);
+
+            image_paths = result["data"]["results"];
+            console.log("image_paths : ", image_paths);
+
+            var photosDiv = document.getElementById("photos_search_results");
+            photosDiv.innerHTML = "";
+
+            var n;
+            for (n = 0; n < image_paths.length; n++) {
+                images_list = image_paths[n]["url"].split('/');
+                imageName = images_list[images_list.length - 1];
+                photosDiv.innerHTML += '<figure class="inline-block"><img src="' + image_paths[n]["url"] + '" style="width:100%"><figcaption>' + imageName + '</figcaption></figure>';
             }
         }).catch(function(result) {
+            var photosDiv = document.getElementById("photos_search_results");
+            photosDiv.innerHTML = "Image not found!";
             console.log(result);
         });
 }
 
-function uploadPhoto() {
-    var fpath = (document.getElementById('uploadedfile').value).split("\\");
-    var fname = fpath[fpath.length - 1];
-    var reader = new FileReader();
-    var file = document.getElementById('uploadedfile').files[0];
-
-    document.getElementById('uploadedfile').value = "";
-    document.getElementById('upload-message').innerHTML = "";
-
-    if ((fpath == "") || (!['png', 'jpg', 'jpeg'].includes(fname.split(".")[1]))) {
-        alert("Invalid file type! Please upload a png/jpg/jpeg file!");
-    }
-    else {
-        file.constructor = () => file;
-        var params = {'filename': fname, 'bucket': 'b2-bucket-photoalbum', 'x-amz-meta-customLabels': customlabels.value, 'Content-Type': file.type, "x-api-key": "aABGiAiHfW5sYOpOejBFQCtOnhWJ1iw3oTQrgpi0"};
-        var additionalparams = {headers: {'x-amz-meta-customLabels': customlabels.value, 'Content-Type': file.type}};
-        reader.onload = function (event) {
-            return apigClient.uploadBucketFilenamePut(params, file, additionalparams)
-            .then(function(result) {
-                document.getElementById('upload-message').innerHTML = "File uploaded successfully!";
-                document.getElementById('customlabels').value = "";
-            })
-            .catch(function(error) {
-                console.log(error);
-            })
-        }
-        reader.readAsBinaryString(file);
-    }
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    // reader.onload = () => resolve(reader.result)
+    reader.onload = () => {
+      let encoded = reader.result.replace(/^data:(.*;base64,)?/, '');
+      if (encoded.length % 4 > 0) {
+        encoded += '='.repeat(4 - (encoded.length % 4));
+      }
+      resolve(encoded);
+    };
+    reader.onerror = (error) => reject(error);
+  });
 }
+
+function uploadPhoto() {
+    var file = document.getElementById('uploaded_file').files[0];
+    console.log(custom_labels.value);
+
+    var file_data;
+    
+    var additionalParams = {
+        headers: {
+            //'Access-Control-Allow-Origin': '*',
+            'x-amz-meta-customLabels': custom_labels.value,
+            'Content-Type': file.type
+        }
+    };
+
+    url = "https://iweisj7vid.execute-api.us-east-1.amazonaws.com/Testing/upload/photo-store-b2/" + file.name;
+    axios.put(url, file, additionalParams).then((response) => {
+    console.log(" New " + response.data);
+    console.log("Success");
+    document.getElementById("uploadText").innerHTML = "IMAGE UPLOADED SUCCESSFULLY!";
+    document.getElementById("uploadText").style.display = "block";
+    document.getElementById("uploadText").style.color = "white";
+    document.getElementById("uploadText").style.fontWeight = "bold";
+    });
+
+    
+//     var encoded_image = getBase64(file).then((data) => {
+//         console.log(data);
+//         var apigClient = apigClientFactory.newClient();
+
+//         var file_type = file.tyFpe + ';base64';
+//         //var file_type = file.type;
+
+//         console.log(file.type);
+
+//   });
+
+//         var body = data;
+//         var params = {
+//         key: file.name,
+//             bucket: 'photo-store-b2',
+//             'Content-Type': file.type,
+//             'x-amz-meta-customLabels': custom_labels.value,
+//             Accept: 'image/*',
+//         };
+//         apigClient
+//         .uploadBucketKeyPut(params, file, additionalParams)
+//         .then(function (res) {
+//             if (res.status == 200) {
+//             document.getElementById('uploadText').innerHTML =
+//                 'Image Uploaded  !!!';
+//             document.getElementById('uploadText').style.display = 'block';
+//             }
+//       });
+//   });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function uploadPhoto() {
+//     let file = document.getElementById('uploaded_file').files[0];
+//     let file_name = file.name;
+//     let file_type = file.type;
+//     let reader = new FileReader();
+
+//     reader.onload = function() {
+//         let arrayBuffer = this.result;
+//         let blob = new Blob([new Int8Array(arrayBuffer)], {
+//             type: file_type
+//         });
+//         let blobUrl = URL.createObjectURL(blob);
+
+
+//         let data = document.getElementById('uploaded_file').files[0];
+//         let xhr = new XMLHttpRequest();
+//         xhr.withCredentials = true;
+//         xhr.addEventListener("readystatechange", function () {
+//             if (this.readyState === 4) {
+//                 console.log(this.responseText);
+//                 document.getElementById('uploadText').innerHTML ='Image Uploaded  !!!';
+//                 document.getElementById('uploadText').style.display = 'block';
+//             }
+//         });
+//         xhr.withCredentials = false;
+//         xhr.open("PUT", "https://xa25mgsax8.execute-api.us-east-1.amazonaws.com/dev/upload/cloud9223-photo-album/"+data.name);
+//         xhr.setRequestHeader("Content-Type", data.type);
+//         xhr.setRequestHeader("x-api-key","hHrLdYpAwDzt03yDa5Io1rVfO56I5Rj8ps7WAbQ1");
+//         xhr.setRequestHeader("x-amz-meta-customLabels", custom_labels.value);
+//         xhr.setRequestHeader("Access-Control-Allow-Origin", '*');
+//         xhr.send(data);
+//     };
+//     reader.readAsArrayBuffer(file);
+// }
+
+
+/*
+function uploadPhoto() {
+    let file = document.getElementById('uploaded_file').files[0];
+    let file_name = file.name;
+    let file_type = file.type;
+    let reader = new FileReader();
+
+    reader.onload = function() {
+        let arrayBuffer = this.result;
+        let blob = new Blob([new Int8Array(arrayBuffer)], {
+            type: file_type
+        });
+        let blobUrl = URL.createObjectURL(blob);
+
+
+        let data = document.getElementById('uploaded_file').files[0];
+        let xhr = new XMLHttpRequest();
+        xhr.withCredentials = true;
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                console.log(this.responseText);
+                document.getElementById('uploadText').innerHTML ='Image Uploaded  !!!';
+                document.getElementById('uploadText').style.display = 'block';
+            }
+        });
+        xhr.withCredentials = false;
+        xhr.open("PUT", "/v1/upload/photos-recognition/"+data.name);
+        xhr.setRequestHeader("Content-Type", data.type);
+        xhr.setRequestHeader("x-api-key","");
+        xhr.setRequestHeader("x-amz-meta-customLabels", custom_labels.value);
+        xhr.setRequestHeader("Access-Control-Allow-Origin", '*');
+        xhr.send(data);
+    };
+    reader.readAsArrayBuffer(file);
+}*/
